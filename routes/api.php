e1e87d56c6112c73ee812f86a87c34a7bf8fa78a
Jeremy <jeremy@chapter4.uk>
2023-08-01 14:17:46 +0100

    REVERT prev change

--- routes/api.php
+++ routes/api.php
@@ -42,7 +42,7 @@ Route::group(['middleware' => ['auth:sanctum']], function () {
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 
-Route::post('/perdulog', [AuthController::class, 'perdulog']);
+Route::post('/perdu/log', [AuthController::class, 'perdulog']);
 
 
 // END
